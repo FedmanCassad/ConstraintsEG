67774afc66ef0dc3bd3bf66c9f054eae76fa6b7b
Vladimir Banushkin <vov_one@icloud.com>
2020-05-31 02:01:01 +0300

    initial commit

---
+++ Course2Week4Task1/Code/PinPadViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PinPadViewController.swift
+//  Course2Week4Task1
+//
+//  Created by Vladimir Banushkin on 31.05.2020.
+//  Copyright © 2020 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class PinPadViewController: UIViewController {
+
+  @IBOutlet weak var buttonFive: RoundButton!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
